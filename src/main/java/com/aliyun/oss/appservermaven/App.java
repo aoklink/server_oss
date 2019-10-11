fddfe765e542723559c9d05ea3c33e248a774ea7
gary.gou <gary.gou@linkfeeling.cn>
2019-10-11 15:16:00 +0800

    update code

--- src/main/java/com/aliyun/oss/appservermaven/App.java
+++ src/main/java/com/aliyun/oss/appservermaven/App.java
@@ -11,31 +11,31 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class App {
-	public static void main(String[] args) throws Exception {
-		// 开启Server，端口号为3990
-		Server server = new Server();
-		int port = 3990; // 默认端口是：3990
-		if (args.length > 0) {
-			port = Integer.parseInt(args[0]); // 通过命令行参数指定监听 端口。
-		}
+    public static void main(String[] args) throws Exception {
+        // 开启Server，端口号为3990
+        Server server = new Server();
+        int port = 3990; // 默认端口是：3990
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]); // 通过命令行参数指定监听 端口。
+        }
 
-		ServerConnector connector = new ServerConnector(server);
-		connector.setPort(port);
-		System.out.println("App server is running on port : " + port);
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        System.out.println("App server is running on port : " + port);
 
-		server.setConnectors(new Connector[] { connector });
-		// 设置handler根路径。
-		ServletContextHandler webApiContext = new ServletContextHandler();
-		webApiContext.setContextPath("/");
-		webApiContext.addServlet(new ServletHolder(CallbackServer.class), "/*");
-		webApiContext.setSessionHandler(new SessionHandler());
+        server.setConnectors(new Connector[]{connector});
+        // 设置handler根路径。
+        ServletContextHandler webApiContext = new ServletContextHandler();
+        webApiContext.setContextPath("/");
+        webApiContext.addServlet(new ServletHolder(CallbackServer.class), "/*");
+        webApiContext.setSessionHandler(new SessionHandler());
 
-		HandlerList handlers = new HandlerList();
-		handlers.setHandlers(new Handler[] { webApiContext, new DefaultHandler() });
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{webApiContext, new DefaultHandler()});
 
-		// 增加handlers并启动server。
-		server.setHandler(handlers);
-		server.start();
-		server.join();
-	}
+        // 增加handlers并启动server。
+        server.setHandler(handlers);
+        server.start();
+        server.join();
+    }
 }
